9173ef8b664fcf3075c7e63d47b5d2355e2d0052
Kaleb Mikiyas <starbouns76@gmail.com>
2024-01-23 17:13:58 +0300

    animation add to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,11 +3,11 @@ import { CiLocationArrow1 } from "react-icons/ci";
 import { BiRightArrowAlt } from "react-icons/bi";
 export default function Hero() {
   return (
-    <section className="flex pt-16 lg:pt-24 ">
+    <section className="flex pt-36">
       <div className=" h-w-10 absolute left-0 top-16 w-16 rotate-90 opacity-30 lg:h-36 lg:w-36">
         <img src="leaf-branch-2.png" alt="leaf" />
       </div>
-      <div className="absolute -right-2 bottom-20 lg:bottom-10  w-16 opacity-30 lg:-right-9 lg:h-36 lg:w-36">
+      <div className="absolute -right-2 bottom-20 w-16  opacity-30 lg:-right-9 lg:bottom-10 lg:h-36 lg:w-36">
         <img src="leaf-branch-4.png" alt="leaf" />
       </div>
       <div className="flex items-start justify-end gap-12 max-lg:flex-col-reverse lg:gap-7">
@@ -15,8 +15,10 @@ export default function Hero() {
           <h1
             className={`${lora.className} flex flex-col gap-4 text-5xl font-medium capitalize leading-[1] text-[#2C2420] max-lg:text-center max-[379px]:text-4xl md:text-5xl lg:text-[55px] xl:text-6xl`}
           >
-            <span>enjoy delicious</span>
-            <span className="flex items-center capitalize max-lg:justify-center">
+            <span className=" animate-slidein opacity-0 [--slidein-delay:300ms]">
+              enjoy delicious
+            </span>
+            <span className="animate-slidein flex items-center capitalize opacity-0 [--slidein-delay:500ms] max-lg:justify-center">
               <img
                 src="home-sushi-title.png"
                 alt="sushi-img"
@@ -26,21 +28,21 @@ export default function Hero() {
             </span>
           </h1>
           <p
-            className={`max-w-[400px] text-center font-medium max-lg:mx-auto lg:text-left ${poppins.className} text-[#9b7564] lg:max-w-[300px]`}
+            className={`max-w-[400px] text-center font-medium max-lg:mx-auto lg:text-left ${poppins.className} animate-slidein text-[#9b7564] opacity-0 [--slidein-delay:700ms] lg:max-w-[300px]`}
           >
             Enjoy a good dinner with the best dishes in the restaurant and
             improve your day.
           </p>
           <div className="max-lg:mx-auto">
             <button
-              className={`flex items-center gap-2 rounded-full px-7 py-4 capitalize text-white ${poppins.className} group bg-orange font-semibold max-lg:text-center`}
+              className={`animate-slidein flex items-center gap-2 rounded-full px-7 py-4 capitalize text-white opacity-0 [--slidein-delay:1000ms] ${poppins.className} group bg-orange font-semibold max-lg:text-center`}
             >
               order now{" "}
               <BiRightArrowAlt className="relative right-0 top-0 h-7 w-7 transition-all group-hover:-right-1" />
             </button>
           </div>
         </div>
-        <div className="max-w-full flex-1 px-5 max-lg:flex-grow-0 sm:px-12 md:px-16">
+        <div className="animate-slidein max-w-full flex-1 px-5 opacity-0 [--slidein-delay:300ms] max-lg:flex-grow-0 sm:px-12 md:px-16">
           <img
             src="home-sushi-rolls2.png"
             className="mx-auto h-auto -rotate-6 object-contain lg:w-full "
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className={` bg-[#FFF1EB] relative min-h-screen overflow-x-hidden px-4 sm:px-10  md:px-16 xl:px-32 flex items-center`}>
+    <main className={` bg-[#FFF1EB] relative min-h-screen overflow-x-hidden px-4 sm:px-10  md:px-16 xl:px-32  `}>
       <Header />
       <Hero />
     </main>
