c6590bb034e61fd6048dd73b1cb8f1bbedc553a2
Kaleb Mikiyas <starbouns76@gmail.com>
2024-01-27 12:07:00 +0300

    food detail's initialized

--- components/FoodDetail.tsx
+++ components/FoodDetail.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { lora, poppins } from "@/app/fonts";
+import Image, { StaticImageData } from "next/image";
+import { CiShoppingCart } from "react-icons/ci";
+interface Props {
+  imgUrl: StaticImageData;
+  title: string;
+  price: number;
+}
 
-function FoodDetail() {
-  return <div>FoodDetail</div>;
+function FoodDetail({ imgUrl, title, price }: Props) {
+  return (
+    <div className="relative flex w-72 py-4 flex-col items-center justify-center gap-1 rounded-lg bg-white pt-28 text-center shadow-xl">
+      <Image
+        src={imgUrl}
+        alt="food-image"
+        className="absolute -top-12 mx-auto"
+        width={230}
+      />
+      <div>
+        <h3 className={`${lora.className} text-2xl font-semibold`}>{title}</h3>
+        <p className={`${poppins.className} mb-2 font-semibold text-[#7F6C64]`}>
+          Japanese Dish
+        </p>
+      </div>
+      <div className={`${poppins.className} relative flex items-center gap-10`}>
+        <p className=" text-orange text-xl font-semibold">${price}</p>
+        <div className="absolute -right-24 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-orange text-center text-white transition hover:shadow-md">
+          <CiShoppingCart className="h-5 w-5" strokeWidth={1.4} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FoodDetail;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 import heroImg from "../public/home-sushi-rolls2.png";
 export default function Hero() {
   return (
-    <section className="flex min-h-screen justify-center pt-36 text-center ">
-      <div className="flex items-center justify-end  gap-20 max-lg:flex-col-reverse lg:gap-7">
+    <section className=" flex min-h-screen justify-center pt-36 text-center  ">
+      <div className="flex items-center justify-end gap-20 max-lg:flex-col-reverse lg:gap-7">
+
         <div className="flex flex-col gap-7  ">
           <h1
             className={`${lora.className} flex flex-col gap-4 text-5xl font-medium capitalize leading-[1] text-[#2C2420] max-lg:text-center max-[379px]:text-4xl md:text-5xl lg:text-[55px] xl:text-6xl`}
@@ -33,7 +34,7 @@ export default function Hero() {
           </p>
           <div className="max-lg:mx-auto">
             <button
-              className={`flex animate-slidein items-center gap-2 rounded-full px-7 py-4 capitalize text-white opacity-0 [--slidein-delay:1000ms] ${poppins.className} group bg-orange font-semibold max-lg:text-center`}
+              className={`flex animate-slidein items-center gap-2 rounded-full px-7 py-4 capitalize text-white opacity-0 [--slidein-delay:1000ms] ${poppins.className} group bg-orange font-semibold outline-none max-lg:text-center`}
             >
               order now{" "}
               <BiRightArrowAlt className="relative right-0 top-0 h-7 w-7 transition-all group-hover:-right-1" />
